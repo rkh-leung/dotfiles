28780c9b35569a6435ff430996336ca4df8312af
rkh-leung <leptanian@gmail.com>
2023-04-03 07:43:17 +1200

    Add toggle lsplines, remap keys to terminal window

--- nvim/lua/leptanian/remap.lua
+++ nvim/lua/leptanian/remap.lua
@@ -47,3 +47,17 @@ vim.api.nvim_set_keymap('n', '?', ':Comment<CR>', { noremap = true, silent = fal
 
 -- Change the visual mode multiline comment
 vim.api.nvim_set_keymap('v', '?', ':CommentMore<CR>', { noremap = true, silent = false })
+
+vim.keymap.set("", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+
+-- Go to tab by number
+vim.keymap.set('n', '<leader>1', '1gt')
+vim.keymap.set('n', '<leader>2', '2gt')
+vim.keymap.set('n', '<leader>3', '3gt')
+vim.keymap.set('n', '<leader>4', '4gt')
+vim.keymap.set('n', '<leader>5', '5gt')
+vim.keymap.set('n', '<leader>6', '6gt')
+vim.keymap.set('n', '<leader>7', '7gt')
+vim.keymap.set('n', '<leader>8', '8gt')
+vim.keymap.set('n', '<leader>9', '9gt')
+vim.keymap.set('n', '<leader>0', ':tablast<cr>')
